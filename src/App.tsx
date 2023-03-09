265a07a6ab0adf860a51630646ce67391fbbf324
brian <brainmccollum@unm.edu>
2023-03-09 00:54:49 -0700

    added navbar scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Navbar from "./components/Navbar"
 
 export default function App() {
+  function logScroll() {
+
+  }
+
+
   return (
     <>
       <Navbar />
       <div className="flex flex-col justify-center items-center">
-        <div className="w-full h-screen bg-white"></div>
+        <div id="home" className="w-full h-screen bg-white"></div>
         <div id="projects" className="w-full h-screen bg-red-500"></div>
         <div id="about" className="w-full h-screen bg-blue-500"></div>
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
 export default function Navbar() {
+
+  function smoothScroll(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, id: string) {
+    e.preventDefault();
+    let div = document.getElementById(id)
+    div && div.scrollIntoView({ behavior: "smooth" })
+  }
+
   return (
     <nav className="fixed left-0 top-0 m-0 w-full">
       <div className="flex flex-row justify-center items-center">
-        <a href="#" className="mx-4">Home</a>
-        <a href="#projects" className="mx-4">Projects</a>
-        <a href="#about" className="mx-4">About</a>
+        <a href="" onClick={(e) => smoothScroll(e, "home")} className="mx-4">Home</a>
+        <a href="" onClick={(e) => smoothScroll(e, "projects")} className="mx-4">Projects</a>
+        <a href="" onClick={(e) => smoothScroll(e, "about")} className="mx-4">About</a>
       </div>
     </nav>
   )
